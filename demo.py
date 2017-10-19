ac6c2c5b9faba27e5d059478f66b21e3c499d0ed
yueyt <yyt030@gmail.com>
2017-10-19 11:22:45 +0800

    update

--- demo.py
+++ demo.py
@@ -11,62 +11,59 @@ from PIL import Image
 from wordcloud import WordCloud, STOPWORDS
 
 
-def get_words(text):
-    return jieba.lcut(text)
+def without_stopwords(words):
+    with open('data/stop_words.txt', encoding='utf8') as f:
+        stop_words = f.read()
+        for k in stop_words:
+            words.pop(k, None)
+
+    words.pop(' ', None)
+    return words
 
 
-def get_words_with_stopwords(text, topK=10000):
-    jieba.analyse.set_stop_words("data/stop_words.txt")
-    tags = jieba.analyse.extract_tags(text, topK=topK)
-    return tags
+def get_words_without_stopwords(text, topK=20):
+    jieba.analyse.set_idf_path("data/SogouLabDic.dic")
+    tags = jieba.analyse.extract_tags(text, topK=topK, withWeight=True, allowPOS=())
+    return dict(tags)
 
 
-def get_word_count(words, num=20):
-    c = Counter(words)
-    result = {word: freq for word, freq in c.most_common(num)}
-    return result
+def get_word_textrank(text, topK=20):
+    tags = jieba.analyse.textrank(text, topK=topK, withWeight=True, allowPOS=('ns', 'n', 'vn', 'v'))
+    return dict(tags)
 
 
-def get_word_textrank(text, topK=20, withWeight=False):
-    tags = jieba.analyse.textrank(text, topK=topK, withWeight=withWeight, allowPOS=('ns', 'n', 'vn', 'v'))
-    return tags
-
-
-def create_word_cloud(word_freq, max_words=300):
-    # read the mask image
-    # taken from
-    # http://www.stencilry.org/stencils/movies/alice%20in%20wonderland/255fk.jpg
+def create_word_cloud(word_freq, to_file='alice.png', max_words=300):
     alice_mask = np.array(Image.open('pic/map.jpeg'))
 
     stopwords = set(STOPWORDS)
     # stopwords.add("said")
 
     wc = WordCloud(background_color="black", max_words=max_words, mask=alice_mask,
-                   stopwords=stopwords, max_font_size=80,
-                   random_state=42, font_path='/System/Library/Fonts/PingFang.ttc')
+                   stopwords=stopwords, max_font_size=80, random_state=42,
+                   font_path='/System/Library/Fonts/PingFang.ttc')
     # generate word cloud
     # wc.generate_from_text(text)
     wc.generate_from_frequencies(word_freq)
 
     # store to file
-    wc.to_file("alice.png")
-
-    # show
-    # plt.imshow(wc, interpolation='bilinear')
-    # plt.axis("off")
-    # plt.figure()
-    # plt.imshow(alice_mask, cmap=plt.cm.gray, interpolation='bilinear')
-    # plt.axis("off")
-    # plt.show()
+    wc.to_file(to_file)
 
 
 if __name__ == '__main__':
+    max_num = 200
     with open('data/xi.txt', encoding='utf8') as f:
         text = f.read()
-        words = get_words(text)
-        most_words = get_word_count(words, num=100)
+        jieba.load_userdict("data/SogouLabDic.dic")
+        words = jieba.lcut(text)
+        most_words = Counter(words).most_common(max_num)
+        most_words = without_stopwords(dict(most_words))
         print('>>>>', most_words)
-        tags = get_word_textrank(text, topK=500, withWeight=True)
-        # print(tags)
-        tags = {word: freq for word, freq in tags}
-        create_word_cloud(tags, max_words=500)
+        create_word_cloud(most_words, to_file='common.png', max_words=max_num)
+        # idf
+        most_words2 = get_words_without_stopwords(text)
+        print('>>> TF-IDF  ', most_words2)
+        create_word_cloud(most_words2, to_file='tf-idf.png', max_words=max_num)
+        # text rank
+        most_words3 = get_word_textrank(text, topK=max_num)
+        print('>>> textRank', most_words3)
+        create_word_cloud(most_words3, to_file='textrank.png', max_words=max_num)
